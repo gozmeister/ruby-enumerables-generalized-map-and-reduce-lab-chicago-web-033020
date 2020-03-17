84c725af80500ad28a8ca9fb91d8b3f46e7b0f5a
Sam Gorick <samgorick.sg@gmail.com>
2020-03-17 21:41:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,8 +12,9 @@ def reduce(array, starting_point = nil)
 i = 0
 if starting_point
   total = starting_point
+else 
+  total = array[i]
 end
-
 while i < array.length do
   total = yield(total, array[i])
   i += 1
